2266a67b7e3961a709b920a3ea95a1defaeffcb0
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2023-01-16 12:11:39 -0700

    initial commit

---
+++ Assignment1/Question7/Question7.py
@@ -0,0 +1,16 @@
+# Part 1
+import math
+
+try: 
+    power = int(input("Please enter an integer: "))
+except:
+    print("Invalid Input, Please try again!")
+ans = pow(3,power)
+print(f"The result is {ans}")
+    
+
+# def main():
+#     pow3
+
+# if __name__ == '__main__':
+#     main()
